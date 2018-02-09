ece399cc1ae0784ab4274f05346f6398a8d89f02
Sajeewa Dayaratne <sajmail@gmail.com>
2018-02-09 22:52:13 +1300

    Added connection to home assistant

--- garage-door.py
+++ garage-door.py
@@ -1,14 +1,38 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-import signal 
+import signal
+import requests 
+import json
 
 #Configuration:
 
 #GPIO Pin used to read door sensor
 DOOR_SENSOR_PIN = 18
 
+#Home Assistant URL
+HOME_ASSISTANT_URL = "http://parrot:8123"
+HOME_ASSISTANT_PASSWORD = ""
+HA_SENSOR_NAME = "garagedoor"
+HA_SENSOR_FRIENDLY_NAME = "Garage Door"
 
+def notifyHomeAssistant(state):
+
+    if ( state != "open" and state != "closed"):
+        print "Unknown state passed to notifyHomeAssistant - " + state
+        return
+
+    icon = "mdi:garage"
+
+    if ( state == "open" ):
+        icon = "mdi:garage-open"
+
+    post_url = "{server}/api/states/binary_sensor.{name}".format( server = HOME_ASSISTANT_URL, name = HA_SENSOR_NAME)
+
+    response = requests.post(post_url, headers={'x-ha-access': HOME_ASSISTANT_PASSWORD, 'content-type': 'application/json'},
+        data=json.dumps({'state': state, 'attributes': {'friendly_name': HA_SENSOR_FRIENDLY_NAME, 'icon': icon}}))
+
+    print(response.text) # For debugging
 
 # Clean up when the user exits with keyboard interrupt
 def cleanup(signal, frame): 
@@ -38,8 +62,10 @@ if __name__ == "__main__":
         if ( isOpen != oldIsOpen ):
             if (isOpen):
                 print "Door is Open"
+                notifyHomeAssistant("open")
             else:
                 print "Door is Closed"
+                notifyHomeAssistant("closed")
     
         # Keep this relatively long to handle debounce so the sensor
         # doesn't flicker between the two states
